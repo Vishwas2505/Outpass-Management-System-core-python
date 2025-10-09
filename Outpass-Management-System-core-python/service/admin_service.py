1d282544170e1d92f7c4304ce057751fdfe17dac
Vishwas2505 <bunnynalumasu07@gmail.com>
2025-10-09 17:40:53 +0530

    Delete Outpass-Management-System-core-python directory

--- Outpass-Management-System-core-python/service/admin_service.py
+++
@@ -1,8 +0,0 @@
-from dao.user_dao import UserDAO
-from dao.admin_dao import AdminDAO
-
-class AdminService:
-    @staticmethod
-    def add_admin(name, email, phone, department, password):
-        user_id = UserDAO.add_user(name, email, phone, department, "admin", password)
-        return AdminDAO.add_admin(user_id)
--- Outpass-Management-System-core-python/service/warden_service.py
+++
@@ -1,8 +0,0 @@
-from dao.user_dao import UserDAO
-from dao.warden_dao import WardenDAO
-
-class WardenService:
-    @staticmethod
-    def add_warden(name, email, phone, department, password, employee_id):
-        user_id = UserDAO.add_user(name, email, phone, department, "warden", password)
-        return WardenDAO.add_warden(user_id, employee_id)
